31f9458698cdfd3820f8fcf102a1df254aadd948
GANZO9055 <kosta.maslihov@gmail.com>
2024-09-15 23:03:24 +0300

    7. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -7,7 +7,10 @@ public class Candidate {
     private int id;
     private String name;
     private String description;
-    private LocalDateTime createDate;
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    public Candidate() {
+    }
 
     public Candidate(int id, String name, String description, LocalDateTime createDate) {
         this.id = id;
